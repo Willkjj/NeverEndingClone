a250d6ef20dea7a4eee04a2eb8365b0760da937a
James Fawcett <jbfawcett24@gmail.com>
2026-09-25 13:39:41 -0600

    create world clock, show example in WorldState on how to hook in

--- WorldState.cs
+++ WorldState.cs
@@ -2,12 +2,28 @@ using Godot;
 
 public partial class WorldState : Node
 {
-    public static WorldState Instance { get; private set; }
-    public TileData[,] Tiles;
+  public static WorldState Instance { get; private set; }
+  public TileData[,] Tiles;
 
-    public override void _Ready()
+  public override void _Ready()
+  {
+    Instance = this;
+    Tiles = new TileData[256, 256];
+
+    Clock.Instance.Tick += OnClockTick;
+  }
+
+  private void OnClockTick(int tickCount)
+  {
+    GD.Print($"Clock tick fired: {tickCount}");
+  }
+
+  // Remove the event listener if worldstate is ever unloaded (it shouldn't)
+  public override void _ExitTree()
+  {
+    if (Clock.Instance != null)
     {
-        Instance = this;
-        Tiles = new TileData[256, 256];
+      Clock.Instance.Tick -= OnClockTick;
     }
+  }
 }
