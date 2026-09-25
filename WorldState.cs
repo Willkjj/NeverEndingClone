70b7b942a0bdbcdae81abaef2e041285fe2c9f74
James Fawcett <jbfawcett24@gmail.com>
2026-09-25 13:48:20 -0600

    fix my idiot merge

--- WorldState.cs
+++ WorldState.cs
@@ -2,20 +2,24 @@ using Godot;
 
 public partial class WorldState : Node
 {
-	public static WorldState Instance { get; private set; }
-	public TileData[,] Tiles;
-	public uint StartingPopulation { get; private set;}
-	public ushort CurrentYear { get; private set;}
-	
-	
+    public static WorldState Instance { get; private set; }
+    public TileData[,] Tiles;
+    public uint StartingPopulation { get; private set; }
+    public ushort CurrentYear { get; private set; }
 
-	public override void _Ready()
-	{
-		Instance = this;
-		Tiles = new TileData[256, 256];
+    public override void _Ready()
+    {
+        Instance = this;
+        Tiles = new TileData[256, 256];
 
-		StartingPopulation = 250;
-		CurrentYear = 0;
+        StartingPopulation = 250;
+        CurrentYear = 0;
 
-	}
+        Clock.Instance.Tick += OnClockTick;
+    }
+
+    private void OnClockTick(int tickNumber)
+    {
+        GD.Print($"Clock tick fired: {tickNumber}");
+    }
 }
